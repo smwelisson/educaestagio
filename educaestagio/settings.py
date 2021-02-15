d01cd71c24d787dd39ea7baae82939c6ba871520
Welisson <smwelisson@gmail.com>
2021-02-15 02:08:13 -0300

    static_url

--- educaestagio/settings.py
+++ educaestagio/settings.py
@@ -123,10 +123,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = 'core/static/'
-# testar: https://educastatic-3fbe8.web.app/
 # STATIC_URL = 'https://educastatic-3fbe8.firebaseapp.com/'
+
+
 STATIC_URL = 'core/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
