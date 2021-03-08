d1acce557cb81cd2686bd25de1c1a6c61c7640ef
Welisson <smwelisson@gmail.com>
2021-03-08 17:52:41 -0300

    arquivos firebase e gitignore

--- educaestagio/settings.py
+++ educaestagio/settings.py
@@ -130,8 +130,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_URL = 'https://educastatic-3fbe8.firebaseapp.com/'
-STATIC_URL = '/static/'
+STATIC_URL = 'https://educastatic-3fbe8.web.app'
+
+# STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
