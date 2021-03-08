21d81933eee4868fdc269b049d9336435423340c
Welisson <smwelisson@gmail.com>
2021-03-08 20:15:02 -0300

    Erro url statics

--- educaestagio/settings.py
+++ educaestagio/settings.py
@@ -131,9 +131,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_URL = 'https://educastatic-3fbe8.web.app'
-
-STATIC_URL = '/static/'
+STATIC_URL = 'https://educastatic-3fbe8.web.app'
+# STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
